b950727dfa90eed919003a71836b034bdaba48e7
mtgv999 <petermgcz@gmail.com>
2024-03-29 23:41:44 +0900

    c

---
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -0,0 +1,16 @@
+package zerobase.weather.domain;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+@Entity @Setter @Getter
+@NoArgsConstructor
+public class Diary {@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; private String weather;
+    private String icon; private double temperature;
+    private String text; private LocalDate date;}
